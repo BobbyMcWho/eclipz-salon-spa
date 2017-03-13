892de91849e197c2939c1c1852baec4a7be41ad3
Robert McDonald <contactrmcdonald@gmail.com>
2017-03-13 18:00:16 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        var h = $('.hero').height() + 50;
+        $('.hero').height(h);
+        var m = 0.4 * h - 40;
+        $('.title').css('margin-top', m + 'px');
+
+    $('.navbar').affix({
+        offset: {
+            top: function() {
+                return $('.hero').height() ;
+            }
+        }
+    });
+  }
+  else {
+    $('.navbar').affix({
+        offset: {
+            top: function() {
+                return $('.hero').height() ;
+            }
+        }
+    });
+  }
+    var sections = $('section'), nav = $('nav'), nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+  nav.find('a').blur();
+});
+
+$('.downArrow, .nav a,.navbar-brand').on('click', function () {
+  var $el = $(this), id = $el.attr('href');
+
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - nav_height
+  }, 500);
+  $el.blur();
+
+  return false;
+});
+$('[data-toggle="tooltip"]').tooltip();
+
+
+$('.maps').click(function () {
+    $('.maps iframe').css("pointer-events", "auto");
+});
+
+$( ".maps" ).mouseleave(function() {
+  $('.maps iframe').css("pointer-events", "none"); 
+});
+
+});
